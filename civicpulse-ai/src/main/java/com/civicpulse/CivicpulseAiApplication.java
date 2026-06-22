c372fd0e2ea8034bc563a495ea0ed1ef9ef91a60
Divyansh Soni <divyanshsoni9a@gmail.com>
2026-06-22 22:36:22 +0530

    Initial Spring Boot setup for CivicPulse AI

---
+++ civicpulse-ai/src/main/java/com/civicpulse/CivicpulseAiApplication.java
@@ -0,0 +1,11 @@
+package com.civicpulse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CivicpulseAiApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(CivicpulseAiApplication.class, args);
+	}
+}
